ed34d5ca0cfc6d33d02b08de3db5ac8c7c4eb802
PoonamSampat <poonamkiran@hotmail.com>
2016-10-21 14:14:24 +0530

    Initial Commit

---
+++ CreationOfQueue/CreationOfQueue/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Azure; // Namespace for CloudConfigurationManager
+using Microsoft.WindowsAzure.Storage; // Namespace for CloudStorageAccount
+using Microsoft.WindowsAzure.Storage.Queue; // Namespace for Queue storage types
+using SendGrid.CSharp.HTTP.Client;
+
+
+namespace CreationOfQueue
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+       
+            // Retrieve storage account from connection string.
+            CloudStorageAccount storageAccount = CloudStorageAccount.Parse(
+                CloudConfigurationManager.GetSetting("StorageConnectionString"));
+
+            // Create the queue client.
+            CloudQueueClient queueClient = storageAccount.CreateCloudQueueClient();
+
+            // Retrieve a reference to a container.
+            CloudQueue queue = queueClient.GetQueueReference("myqueue");
+
+            queue.CreateIfNotExists();
+
+            // Create a message and add it to the queue.
+            CloudQueueMessage message = new CloudQueueMessage("Wait on Success Message");
+            queue.AddMessage(message);
+
+        }
+    }
+}
